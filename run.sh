c49548dcf9448f5b0e9a6b95463c277699323b9e
nobreakfast <haocheng.zhao@hotmail.com>
2024-06-24 15:04:37 +0800

    add config

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/camera.py 4
 CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/lidar.py 4
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/fusion.py 4
 CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/radar.py 4
+CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/fusion.py 4
+CUDA_VISIBLE_DEVICES=0,1,2,3 ./tools/dist_train.sh configs/vod/bevfusion/fusion_lc.py 4
